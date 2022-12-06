af14290968e8115a55b1f484728306c0b07db66c
Brandon Smith <smithb@wlwv.k12.or.us>
2022-12-06 08:21:13 -0800

    intial commit

---
+++ src/WhileLoopFun.java
@@ -0,0 +1,36 @@
+
+/*
+    Template for while loop
+
+    //initialize loop control variable
+    while ( some boolean expression) {
+        //loop stuff accessed if boolean expression true
+        //more loop stuff
+        ...
+        //update loop control variable
+ */
+
+
+public class WhileLoopFun {
+
+    public static void main(String[] args) {
+        //count to 10
+
+
+        //what does this code loop do? Do this without running it
+        int count = 2;
+        while(count <= 100){
+            System.out.print(count + " ");
+            count *= 2;
+        }
+        System.out.println();
+        //write a loop that counts by 5 until you get to 100
+
+        //write a loop that counts down from 10 to 1 then prints "Blast off!"
+
+        //loop mistakes and while vs do while loop
+        //make an infinite loop happen
+
+
+    }
+}
